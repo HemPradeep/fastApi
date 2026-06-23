324504b92baa4fd0ee51229bdf5975255eb6b351
Hem Pradeep Royal Thummala <pradeeproyalthummala@Hems-MacBook-Pro.local>
2026-06-23 12:58:20 +0530

    runs the docker container with necessary modifications

--- models/user_db.py
+++ models/user_db.py
@@ -9,11 +9,11 @@ load_dotenv()
 class UserDataManager:
     def __init__(self):
         self.conn = psycopg2.connect(
-            host=os.getenv("DB_HOST", "localhost"),
-            port=int(os.getenv("DB_PORT", 5432)),
-            database=os.getenv("DB_NAME", "userdb"),
-            user=os.getenv("DB_USER", "testuser"),
-            password=os.getenv("PASSWORD", "password"),
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("PASSWORD"),
         )
         self.cursor = self.conn.cursor()
         self.create_user_table()
